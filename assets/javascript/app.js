0438950742aa3dc9c472948e978d4ea35be86867
KimblesAndBits <wearethekimbles@gmail.com>
2019-05-30 19:21:10 -0700

    working restaurant pull

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,32 +1,62 @@
 // Javascript for Nav bar 
-$(document).ready(function() {	
-    $(".menu-trigger").click(function() {
-        $(".nav-menu").slideToggle(400, function() {
+$(document).ready(function () {
+    $(".menu-trigger").click(function () {
+        $(".nav-menu").slideToggle(400, function () {
             $(this).toggleClass("nav-expanded").css('display', '');
-        }); 
+        });
     });
-    
+
+    var baseURL = "https://developers.zomato.com/api/v2.1/";
+    var cityID = 0;
     var formReady = false;
     var yourMood = "";
     var yourFood = "";
     var yourCity = "";
     var yourState = "";
+    var locations = "";
+    var foodChoices = "";
 
-    $("choices-form").on("submit", function() {
+    $("choices-form").on("submit", function () {
         event.preventDefault();
     })
 
-    $("#submit-button").on("click", function(){
+    $("#submit-button").on("click", function () {
         checkForm();
-        if(formReady) {
+        if (formReady) {
             console.log("CLICKED IT!");
             getInfo();
             formReady = false;
+            locations = baseURL + "locations?query=" + yourCity + "%2C" + yourState;
+            console.log("ready!"),
+                $.ajax({
+                    type: "GET",
+                    beforeSend: function (request) {
+                        request.setRequestHeader("user-key", 'fc5e13c945185137ec8e98446ea28a62');
+                    },
+                    url: locations,
+                    success: function (msg) {
+                        console.log('mes', msg.location_suggestions[0].entity_id);
+                        cityID = msg.location_suggestions[0].entity_id;
+                        foodChoices = baseURL + "search?entity_id=" + cityID + "&entity_type=city&radius=5000&cuisines=" + yourFood;
+                        console.log("ready!"),
+                            $.ajax({
+                                type: "GET",
+                                beforeSend: function (request) {
+                                    request.setRequestHeader("user-key", 'fc5e13c945185137ec8e98446ea28a62');
+                                },
+                                url: foodChoices,
+                                success: function (msg) {
+                                    console.log('mes', msg);
+
+                                }
+                            });
+                    }
+                });
         }
     });
 
     function checkForm() {
-        if($("#moodInput").val() && $("#foodInput").val() && $("#inputCity").val() && $("#inputState").val()) {
+        if ($("#moodInput").val() && $("#foodInput").val() && $("#inputCity").val() && $("#inputState").val()) {
             formReady = true;
             console.log("Form Ready!");
         }
@@ -42,4 +72,9 @@ $(document).ready(function() {
         $("#inputCity").val("");
         $("#inputState").val("");
     }
-}); 
+
+
+
+
+
+});
